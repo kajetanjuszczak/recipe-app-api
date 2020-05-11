c3928a7d9c671f839346e4073560e7dc4de4362d
Kajetan <juszczak.kajetan@gmail.com>
2020-05-11 17:50:17 +0200

    Added create superuser function

--- app/core/models.py
+++ app/core/models.py
@@ -6,8 +6,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -18,3 +18,14 @@ class ModelTests(TestCase):
         email = "user@DOMAIN.COM"
         user = get_user_model().objects.create_user(email, "Test123")
         self.assertEqual(user.email, email.lower())
+
+    def test_user_registering_with_no_email_throws_error(self):
+        email = ""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email, "Test123")
+
+    def test_superuser_is_created_with_is_superuser_and_is_staff_flag(self):
+        user = get_user_model().objects.create_superuser(
+            "email@outlook.com", "Test123")
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
