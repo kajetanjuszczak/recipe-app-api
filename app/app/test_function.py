8980e1f7a00abbe5044a5b8e84c9e071a443dc8a
Kajetan <juszczak.kajetan@gmail.com>
2020-05-11 16:39:12 +0200

    added extended user model with email instead of username.

--- app/app/test_function.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from app.functions import addition
-
-
-class FunctionTests(TestCase):
-
-    def test_function(self):
-        self.assertEqual(addition(2,3), 5)
