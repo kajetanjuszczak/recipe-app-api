6e1d1a49440a2d4dcaab48d0f624498a9ecd06a8
Kajetan <juszczak.kajetan@gmail.com>
2020-05-25 22:51:17 +0200

    added DB +  handling iin a form of wait for db

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,11 @@ MAINTAINER Kajetan
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requrements.txt
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .temp-build-deps \
+      gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r /requrements.txt
-
+RUN apk del .temp-build-deps
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,19 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Waiting for db connection')
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('Database not available, waiting 1 sec')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database Available!'))
